def2d67e65f4a39248ffcdbfc480a40cabeb0ba7
NTorres67 <ntorres67@sbcglobal.net>
2018-03-09 13:21:10 -0500

    Inital Commit

---
+++ TravelRevewApp/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelRevewApp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+        public string Type { get; set; }
+
+        // Navigation Property
+        public virtual ICollection<Review> Reviews { get; set; }
+    }
+}
---
+++ TravelRevewApp/Models/Review.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TravelRevewApp.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewID { get; set; }
+        [Display(Name = "Location or Organization")]
+        public string Name { get; set; }
+        [Display(Name = "Arrival Date")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy")]
+        public DateTime Arrive { get; set; }
+        [Display(Name = "Departure Date")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy")]
+        public DateTime Depart { get; set; }
+        public bool Sunny { get; set; }
+        public string Rating { get; set; }
+        public string Comments { get; set; }
+        
+
+        //Foreign Keys and Navigation Properties\
+        [ForeignKey("Category"), Display(Name = "Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+    }
+}
